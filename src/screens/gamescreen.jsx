469197fabc419394139bbe176d27197988f9f51d
himanshucode-07 <himanshusinghthakur23@gmail.com>
2026-03-30 20:54:03 +0530

    feat: add ScenarioCard and display current scenario

--- src/screens/gamescreen.jsx
+++ src/screens/gamescreen.jsx
@@ -1,3 +1,12 @@
-export default function GameScreen() {
-  return <h1>Game</h1>
+export default function GameScreen( {scenarioIndex}) {
+   const scenarios = [
+    { text: "You received your salary ₹5000" },
+    { text: "Your friend invites you to a party" }
+  ]
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h2>{scenarios[scenarioIndex].text}</h2>
+    </div>
+  )
 }
